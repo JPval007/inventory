81f96b57938f111fa1fb7d4487487ede4cd23401
Juan Pablo <juanvalenzuela4business@gmail.com>
2023-11-02 15:49:31 -0600

    Final product

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <info></info>
+    </div>
+  );
+}
+
+function info() {
+  return (
+    <div>
+    <h1>Inventory System</h1>
+    <p>Manage your stuff.</p>
     </div>
   );
 }
